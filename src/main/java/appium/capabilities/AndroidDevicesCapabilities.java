f1bd9b089002dc794de9ce091e6ddb29e703e2df
ibhaka <iihalilkalkan@gmail.com>
2022-07-27 16:46:23 +0300

    first commit

---
+++ src/main/java/appium/capabilities/AndroidDevicesCapabilities.java
@@ -0,0 +1,34 @@
+package appium.capabilities;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+public enum AndroidDevicesCapabilities {
+
+
+    ANDROID_OREO("src/test/resources/capabilities/android-oreo.json");
+
+
+    private String path;
+
+    AndroidDevicesCapabilities(String path) {
+        this.path = path;
+    }
+
+    public DesiredCapabilities getAndroidCapabilitiesFromPlataform() {
+        DesiredCapabilities androidCapabilities = Load_capabilities.pathToDesiredCapabilitites(this.path);
+        androidCapabilities.setCapability("app", new File("src/test/resources/apps/app-debug.apk").getAbsolutePath());
+
+       // androidCapabilities.setCapability("app", new File("C:/Users/ibhaka/Downloads/kidly118.apk").getAbsolutePath());
+
+        return androidCapabilities;
+    }
+
+    public static void showAvaliableAndroidDevices() {
+        System.out.println("======= ANDROID DEVICES ====== ");
+        for (AndroidDevicesCapabilities androidDevicesCapabilities : AndroidDevicesCapabilities.values()) {
+            System.out.println(androidDevicesCapabilities.name());
+        }
+    }
+}
